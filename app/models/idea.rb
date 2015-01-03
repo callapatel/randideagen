edea898c829bb3220be1978e4af2e011b34dff53
Calla <callapatel@gmail.com>
2015-01-02 23:08:03 -0700

    so tired but the active record is working

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,8 +1,12 @@
 class Idea < ActiveRecord::Base
   def self.random
-    rand_id = rand(Idea.count) + 1
-    Idea.find(rand_id).name
+    #rand_id = rand(Idea.count) + 1
+    #Idea.find(rand_id).name
+    Idea.limit(1).order("RANDOM()").each do |one|
+      @huh = one.name
+    end
+    return @huh
   end
 
-  
+
 end
